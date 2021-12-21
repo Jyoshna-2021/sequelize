edf75e3c57f0dd6ca18c3870c4c2d31f5498243a
Jyoshna-2021 <91448597+Jyoshna-2021@users.noreply.github.com>
2021-12-21 11:23:44 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+var express = require("express");
+const {sequelize}=require("./models");
+const {getadmins,postadmin}=require("./controller/admintablecontroller");
+const { getuser, postuser } = require("./controller/usertablecontroller.js");
+const {gettranscation,posttranscation,deleteBook}=require("./controller/booktranscationcontroller")
+const { getbook, postbook } = require("./controller/bookdetailscontroller");
+const app=express();
+const cors=require('cors');
+
+app.use(cors({
+    origin:"*"
+}));
+app.use(express.json());
+
+app.get("/admins",getadmins);
+app.post("/admins",postadmin);
+app.get("/users",getuser);
+app.post("/users",postuser);
+app.get("/transcation",gettranscation);
+app.post("/transcation",posttranscation)
+app.delete("/transcation/:BookId",deleteBook);
+app.get("/books",getbook);
+app.post("/books",postbook);
+const PORT=6000;
+app.listen({port:PORT},async()=>
+   {console.log(`server running ${PORT}`)
+   try{
+   await sequelize.authenticate();
+   }
+   catch(e){
+       console.log(e);
+   }
+});
